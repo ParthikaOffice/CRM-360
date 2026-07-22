d4ba334731c1e2d51770caebd7ddea4f2eba0c34
ParthikaOffice <parthika.mahajan@itsoftlab.com>
2026-07-22 15:05:33 +0530

    2FA backend bug

--- backend/test-mail.js
+++ backend/test-mail.js
@@ -1,15 +1,25 @@
 require("dotenv").config();
 const nodemailer = require("nodemailer");
-
+const dns = require("dns");
 async function test() {
   const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-      user: process.env.SMTP_USER,
-      pass: process.env.SMTP_PASS,
-    },
-  });
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
 
+  auth: {
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASS,
+  },
+
+  tls: {
+    servername: "smtp.gmail.com",
+  },
+
+  lookup: (hostname, options, callback) => {
+    dns.lookup(hostname, { family: 4 }, callback);
+  },
+});
   try {
     await transporter.verify();
     console.log("✅ SMTP Login Successful");
